61f5bdc23d2f9d2423621f560958d2218296d439
Mark Ubiadas <mubiadas@gmail.com>
2023-03-03 09:45:43 -0800

    Added donations - one to many
    update folder

---
+++ javaspring/donations-one-to-many/src/main/java/com/markubiadas/donationsonetomany/DonationsOneToManyApplication.java
@@ -0,0 +1,13 @@
+package com.markubiadas.donationsonetomany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonationsOneToManyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DonationsOneToManyApplication.class, args);
+	}
+
+}
---
+++ javaspring/donations-one-to-many/src/test/java/com/markubiadas/donationsonetomany/DonationsOneToManyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.markubiadas.donationsonetomany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DonationsOneToManyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
